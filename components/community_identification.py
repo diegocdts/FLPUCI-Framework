fb273a73aafdceb854675e23a1a13c23ee602a30
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-08 19:20:44 -0300

    community_identification

---
+++ components/community_identification.py
@@ -0,0 +1,36 @@
+from components.deep_learning_model import FullConvolutionalAutoEncoder
+from components.federated_learning_model import FederatedFullConvolutionalAutoEncoder
+from inner_types.data import Dataset
+from inner_types.learning import LearningApproach, FCAEProperties, TrainingParameters, WindowStrategy
+
+
+def adjust_first_interval(first_interval: int):
+    if first_interval <= 0:
+        first_interval = 1
+    return first_interval
+
+
+class CommunityIdentification:
+
+    def __init__(self, dataset: Dataset,
+                 approach: LearningApproach,
+                 properties: FCAEProperties,
+                 parameters: TrainingParameters,
+                 strategy: WindowStrategy):
+        self.parameters = parameters
+        self.strategy = strategy
+
+        if approach == LearningApproach.CEN:
+            self.model = FullConvolutionalAutoEncoder(dataset, parameters, properties)
+        else:
+            self.model = FederatedFullConvolutionalAutoEncoder(dataset, parameters, properties)
+
+    def model_training(self, first_interval: int, last_interval: int):
+        first_interval = adjust_first_interval(first_interval)
+
+        for end_window in range(first_interval, last_interval):
+            start_window = self.strategy.get_start_window(end_window)
+
+            self.model.training(start_window, end_window)
+
+            encodings, indices = self.model.encoder_prediction(start_window=end_window, end_window=end_window + 1)
