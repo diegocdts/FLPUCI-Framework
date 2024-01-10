effc2fa80cf1ee4aef53fe5d69dccb3223abc293
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-10 16:55:25 -0300

    names

--- inner_functions/names.py
+++ inner_functions/names.py
@@ -3,5 +3,9 @@ def sources():
     return ['all_pairs', 'intra_community', 'inter_community']
 
 
+def curves():
+    return ['lower', 'mean', 'upper']
+
+
 def column_k(k: int):
     return f'k={k}'
