e4efbb52741d735480453766cfd2d7d05385838f
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-10 16:55:19 -0300

    names

--- inner_types/names.py
+++ inner_types/names.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 
 class ExportedFilesName(Enum):
-    TRAINING_LOSS = 'Training losses.csv'
-    TESTING_LOSS = 'Testing losses.csv'
-    LOSSES_CURVE = 'Losses curves.png'
+    TRAINING_LOSS_CSV = 'Training losses.csv'
+    TESTING_LOSS_CSV = 'Testing losses.csv'
+    LOSSES_PNG = 'Losses curves.png'
+
+    CONTACT_TIME_CSV = 'Contact time.csv'
+    MSE_CSV = 'MSE.csv'
+    SSIM_CSV = 'SSIM.csv'
+    ARI_CSV = 'ARI.csv'
