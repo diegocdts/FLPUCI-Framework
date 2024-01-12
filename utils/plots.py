6e5e1b8fb6a2e239773105f06d499332b2500a3a
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-12 18:18:04 -0300

    plots

--- utils/plots.py
+++ utils/plots.py
@@ -3,9 +3,13 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib import colors as mcolors
 
-from inner_functions.names import sources
+from inner_functions.names import sources, choice_method
 from inner_types.learning import LearningApproach
 from inner_types.plots import FigSize, AxisLabel, FontSize, Legend
+
+
+def colors(color_name: str):
+    return mcolors.TABLEAU_COLORS.get(f'tab:{color_name}')
 
 
 def plot_losses(training_losses: np.array, testing_losses: np.array, approach: LearningApproach, path: str):
@@ -75,15 +79,15 @@ def plot_metric(dataframe: pd.DataFrame,
     plt.plot(k_candidates, means, label=intra_community.capitalize())
     plt.fill_between(k_candidates, lower_bounds, upper_bounds, alpha=0.2)
 
-    plt.scatter(k_candidates[index_aic_choice], means[index_aic_choice], color=mcolors.TABLEAU_COLORS.get('tab:orange'))
+    plt.scatter(k_candidates[index_aic_choice], means[index_aic_choice], color=colors('orange'))
     plt.text(k_candidates[index_aic_choice], means[index_aic_choice], f'AIC choice (k={ks_chosen[0]})',
              verticalalignment='bottom', horizontalalignment='center')
 
-    plt.scatter(k_candidates[index_bic_choice], means[index_bic_choice], color=mcolors.TABLEAU_COLORS.get('tab:orange'))
+    plt.scatter(k_candidates[index_bic_choice], means[index_bic_choice], color=colors('orange'))
     plt.text(k_candidates[index_bic_choice], means[index_bic_choice], f'BIC choice (k={ks_chosen[1]})',
              verticalalignment='top', horizontalalignment='center')
 
-    plt.scatter(k_candidates[index_best_choice], means[index_best_choice], color=mcolors.TABLEAU_COLORS.get('tab:orange'))
+    plt.scatter(k_candidates[index_best_choice], means[index_best_choice], color=colors('orange'))
     plt.text(k_candidates[index_best_choice], means[index_best_choice], f'Best choice  (k={ks_chosen[2]})',
              verticalalignment='top', horizontalalignment='center')
 
@@ -96,9 +100,9 @@ def plot_metric(dataframe: pd.DataFrame,
     plt.plot(k_candidates, means, label=inter_community.capitalize())
     plt.fill_between(k_candidates, lower_bounds, upper_bounds, alpha=0.2)
 
-    plt.scatter(k_candidates[index_aic_choice], means[index_aic_choice], color=mcolors.TABLEAU_COLORS.get('tab:green'))
-    plt.scatter(k_candidates[index_bic_choice], means[index_bic_choice], color=mcolors.TABLEAU_COLORS.get('tab:green'))
-    plt.scatter(k_candidates[index_best_choice], means[index_best_choice], color=mcolors.TABLEAU_COLORS.get('tab:green'))
+    plt.scatter(k_candidates[index_aic_choice], means[index_aic_choice], color=colors('green'))
+    plt.scatter(k_candidates[index_bic_choice], means[index_bic_choice], color=colors('green'))
+    plt.scatter(k_candidates[index_best_choice], means[index_best_choice], color=colors('green'))
 
     plt.ylabel(axis_label.value, fontsize=FontSize.DEFAULT.value)
     plt.xlabel(AxisLabel.K.value, fontsize=FontSize.DEFAULT.value)
@@ -107,4 +111,55 @@ def plot_metric(dataframe: pd.DataFrame,
     plt.tight_layout()
 
     plt.savefig(file_path)
+    plt.close()
+
+
+def plot_strategy_comparison(all_pairs_mean: float,
+                             acc_means: pd.DataFrame,
+                             sli_means: pd.DataFrame,
+                             acc_ks_chosen: np.array,
+                             sli_ks_chosen: np.array,
+                             axis_label: AxisLabel,
+                             path: str):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(FigSize.DEFAULT.value)
+
+    labels_per_choice = []
+    for index, choice in enumerate(choice_method()):
+        labels_per_choice.append(f'ACC k ={acc_ks_chosen[index]}\nSLI k={sli_ks_chosen[index]}')
+    labels = [f'{choice_method()[index]}\n{label}' for index, label in enumerate(labels_per_choice)]
+    x_values = np.arange(len(labels))
+    width = 0.1
+
+    # all_pairs
+    plt.axhline(all_pairs_mean, linestyle='dashed', label=sources()[0].capitalize(), color=colors('blue'))
+
+    # intra_community
+    intra_indexes = [0, 2, 4]
+    acc_intra = acc_means[intra_indexes]
+    sli_intra = sli_means[intra_indexes]
+
+    # inter_community
+    inter_indexes = [1, 3, 5]
+    acc_inter = acc_means[inter_indexes]
+    sli_inter = sli_means[inter_indexes]
+
+    ax.bar(x_values - (3 * width / 2), acc_intra, width, color=colors('orange'),
+           label='{} ACC'.format(sources()[1].capitalize()))
+    ax.bar(x_values - (1 * width / 2), acc_inter, width, color=colors('green'),
+           label='{} ACC'.format(sources()[2].capitalize()))
+    ax.bar(x_values + (1 * width / 2), sli_intra, width, color=colors('cyan'),
+           label='{} SLI'.format(sources()[1].capitalize()))
+    ax.bar(x_values + (3 * width / 2), sli_inter, width, color=colors('purple'),
+           label='{} SLI'.format(sources()[2].capitalize()))
+
+    ax.set_ylabel(axis_label.value, fontsize=FontSize.DEFAULT.value)
+    ax.set_xlabel(AxisLabel.K.value, fontsize=FontSize.DEFAULT.value)
+
+    ax.set_xticks(x_values)
+    ax.set_xticklabels(labels, fontsize=FontSize.DEFAULT.value)
+    ax.tick_params(axis='both', which='both', labelsize=FontSize.DEFAULT.value)
+    ax.legend(loc=Legend.BEST_LOCATION.value, fontsize=FontSize.DEFAULT.value)
+    plt.tight_layout()
+    plt.savefig(path)
     plt.close()
