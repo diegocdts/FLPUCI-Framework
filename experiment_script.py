6078928f74496b8e3318d56831d7972904a63d31
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-12 19:33:19 -0300

    experiment_script

--- experiment_script.py
+++ experiment_script.py
@@ -1,12 +1,13 @@
 from components.community_identification import CommunityIdentification
 from instances.arguments import arguments
 
-dataset, approach, properties, parameters, strategy, first_interval, last_interval = arguments()
+dataset, approach, properties, parameters, strategy, first_interval, last_interval, best_metric = arguments()
 
 framework = CommunityIdentification(dataset,
                                     approach,
                                     properties,
                                     parameters,
-                                    strategy)
+                                    strategy,
+                                    best_metric)
 framework.model_training(first_interval, last_interval)
 framework.compare_window_strategies()
