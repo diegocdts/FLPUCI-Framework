f6f4ea8889b0a38c8a488d5f3bf8ec2109c3377c
Diego Souza <souza.claudiodiego@gmail.com>
2024-01-15 17:23:08 -0300

    learning

--- instances/learning.py
+++ instances/learning.py
@@ -3,11 +3,11 @@ from inner_types.learning import FCAEProperties, TrainingParameters
 sfc_rt_properties = FCAEProperties(input_shape=(40, 40, 1),
                                    encode_layers=[128, 64, 32],
                                    encode_activation='relu',
-                                   decode_activation='linear',
+                                   decode_activation='relu',
                                    kernel_size=(3, 3),
                                    encode_strides=[2, 2, 2],
                                    padding='same',
-                                   latent_space=10,
+                                   latent_space=100,
                                    learning_rate=0.0005)
 
 ngsim_properties = FCAEProperties(input_shape=(4, 56, 1),
@@ -17,7 +17,7 @@ ngsim_properties = FCAEProperties(input_shape=(4, 56, 1),
                                   kernel_size=(5, 5),
                                   encode_strides=[2, 2],
                                   padding='same',
-                                  latent_space=10,
+                                  latent_space=20,
                                   learning_rate=0.002)
 
 fed_parameters = TrainingParameters(epochs=3, batch_size=2, shuffle_buffer=10, prefetch_buffer=-1, rounds=20)
