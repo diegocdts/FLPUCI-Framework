fe48a641ad408fb57c42fb306d1a49737e511906
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-20 20:22:48 -0300

    analysis

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 from data_analysis.sample_distribution import SampleAnalysis, heatmap_matrix
-from instances.data import sfc
+from instances.data import sfc, rt, ngsim
 
-analysis = SampleAnalysis(sfc, interval=0, threshold=0.7)
+analysis = SampleAnalysis(ngsim, interval=0)
 pearson_matrix = analysis.pearson_correlation_at_interval()
 kendal_matrix = analysis.kendaltau_correlation_at_interval()
 heatmap_matrix(pearson_matrix, kendal_matrix)
--- data_analysis/sample_distribution.py
+++ data_analysis/sample_distribution.py
@@ -30,12 +30,12 @@ def heatmap_matrix(matrix_pearson: np.array, matrix_kendaltau: np.array):
     fig, axs = plt.subplots(1, 2, figsize=(14, 6))
 
     # Plotar heatmap para a matriz de correlação de Pearson
-    im1 = axs[0].imshow(matrix_pearson, cmap='viridis', interpolation='nearest', vmin=-1, vmax=1)
+    im1 = axs[0].imshow(matrix_pearson, cmap='viridis', interpolation='nearest', vmin=matrix_pearson.min(), vmax=1)
     axs[0].set_title('Matriz de Correlação - Pearson')
     fig.colorbar(im1, ax=axs[0], label='Correlação')
 
     # Plotar heatmap para a matriz de correlação de Kendall-Tau
-    im2 = axs[1].imshow(matrix_kendaltau, cmap='viridis', interpolation='nearest', vmin=-1, vmax=1)
+    im2 = axs[1].imshow(matrix_kendaltau, cmap='viridis', interpolation='nearest', vmin=matrix_kendaltau.min(), vmax=1)
     axs[1].set_title('Matriz de Correlação - Kendall-Tau')
     fig.colorbar(im2, ax=axs[1], label='Correlação')
 
@@ -43,30 +43,59 @@ def heatmap_matrix(matrix_pearson: np.array, matrix_kendaltau: np.array):
     plt.show()
 
 
-def most_correlated(matrix: np.array, threshold: float = 0.8):
+def most_correlated(matrix: np.array):
     total = 0
-    count = 0
+    e0 = 0
+    p25 = 0
+    p50 = 0
+    p75 = 0
+    p1 = 0
+    n25 = 0
+    n50 = 0
+    n75 = 0
+    n1 = 0
     correlations = []
     for i in range(len(matrix)):
         for j in range(i + 1, len(matrix)):
             total += 1
             correlations.append(matrix[i, j])
-            if matrix[i, j] > threshold or matrix[i, j] < -threshold:
-                count += 1
-                #print(f'Variáveis {i} e {j} têm correlação de {matrix[i, j]}')
-    print(f'Ao todo, {count} pares têm correlação inferior a -{threshold} e superior a {threshold}')
-    print(f'Enquanto {total - count} têm correlação entre -{threshold} e {threshold}')
+            if matrix[i, j] == 0.0:
+                e0 += 1
+            elif matrix[i, j] > 0:
+                if matrix[i, j] < 0.25:
+                    p25 += 1
+                elif matrix[i, j] < 0.5:
+                    p50 += 1
+                elif matrix[i, j] < 0.75:
+                    p75 += 1
+                else:
+                    p1 += 1
+            else:
+                if matrix[i, j] > -0.25:
+                    n25 += 1
+                elif matrix[i, j] > -0.5:
+                    n50 += 1
+                elif matrix[i, j] > -0.75:
+                    n75 += 1
+                else:
+                    n1 += 1
+
+    def percent(c):
+        return f'{c*100/total:.2f}%'
+    print(f'Total de pares = {total}\nIguais a 0: {e0}\n--')
+    print(f'Entre 0 e 0.25: {p25} ({percent(p25)})\nEntre 0.25 e 0.50: {p50} ({percent(p50)})\n'
+          f'Entre 0.50 e 0.75: {p75} ({percent(p75)})\nEntre 0.75 e 1: {p1} ({percent(p1)})\n--')
+    print(f'Entre 0 e -0.25: {n25} ({percent(n25)})\nEntre -0.25 e -0.50: {n50} ({percent(n50)})\n'
+          f'Entre -0.50 e -0.75: {n75} ({percent(n75)})\nEntre -0.75 e -1: {n1} ({percent(n1)})\n--')
 
     mean_correlation = np.mean(correlations)
     print(f'A correlação média é de {mean_correlation}\n')
 
 
-
 class SampleAnalysis:
 
-    def __init__(self, dataset: Dataset, interval: int, threshold: float):
+    def __init__(self, dataset: Dataset, interval: int):
         self.dataset = dataset
-        self.threshold = threshold
         self.f3_dm = Path.f3_dm(dataset.name)
         self.data = self.load_interval(interval)
 
@@ -85,7 +114,7 @@ class SampleAnalysis:
     def pearson_correlation_at_interval(self):
         pearson_correlation_matrix = np.corrcoef(self.data)
         print('Pearson Correlation')
-        most_correlated(pearson_correlation_matrix, self.threshold)
+        most_correlated(pearson_correlation_matrix)
         return pearson_correlation_matrix
 
     def kendaltau_correlation_at_interval(self):
@@ -97,5 +126,5 @@ class SampleAnalysis:
                 coeficiente, _ = kendalltau(self.data[i], self.data[j])
                 kendaltau_correlation_matrix[i, j] = coeficiente
         print('Kendaltau Correlation')
-        most_correlated(kendaltau_correlation_matrix, self.threshold)
+        most_correlated(kendaltau_correlation_matrix)
         return kendaltau_correlation_matrix
