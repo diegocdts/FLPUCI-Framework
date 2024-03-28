2579715b16dccb371b8d254b820a24a200b9c551
Diego Souza <souza.claudiodiego@gmail.com>
2024-03-28 13:27:13 -0300

    wip: plot correlations

--- analysis.py
+++ analysis.py
@@ -1,8 +1,6 @@
-from data_analysis.sample_distribution import SampleAnalysis, heatmap_matrix
-from instances.data import sfc, rt, ngsim
+from data_analysis.iid_analysis import get_sample_correlations
+from instances.arguments import arguments
 
-analysis = SampleAnalysis(ngsim, interval=0)
-pearson_matrix = analysis.pearson_correlation_at_interval()
-spearman_matrix = analysis.spearman_correlation_at_interval()
-kendal_matrix = analysis.kendaltau_correlation_at_interval()
-heatmap_matrix(spearman_matrix, kendal_matrix)
+args = arguments()
+
+get_sample_correlations(args.dataset, args.last_interval)
--- data_analysis/iid_analysis.py
+++ data_analysis/iid_analysis.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import kendalltau, spearmanr
+from scipy.stats import kendalltau, spearmanr, t
 
 from components.sample_generation import min_max_scaling
 from inner_functions.path import sorted_files, build_path
@@ -43,61 +43,49 @@ def heatmap_matrix(matrix_spearman: np.array, matrix_kendaltau: np.array):
     plt.show()
 
 
-def most_correlated(matrix: np.array):
-    total = 0
-    e0 = 0
-    p25 = 0
-    p50 = 0
-    p75 = 0
-    p1 = 0
-    n25 = 0
-    n50 = 0
-    n75 = 0
-    n1 = 0
-    correlations = []
-    for i in range(len(matrix)):
-        for j in range(i + 1, len(matrix)):
-            total += 1
-            correlations.append(matrix[i, j])
-            if matrix[i, j] == 0.0:
-                e0 += 1
-            elif matrix[i, j] > 0:
-                if matrix[i, j] < 0.25:
-                    p25 += 1
-                elif matrix[i, j] < 0.5:
-                    p50 += 1
-                elif matrix[i, j] < 0.75:
-                    p75 += 1
-                else:
-                    p1 += 1
-            else:
-                if matrix[i, j] > -0.25:
-                    n25 += 1
-                elif matrix[i, j] > -0.5:
-                    n50 += 1
-                elif matrix[i, j] > -0.75:
-                    n75 += 1
-                else:
-                    n1 += 1
+def mean_correlation(matrix: np.array):
+    np.fill_diagonal(matrix, np.nan)
+    matrix_without_nan = matrix[~np.isnan(matrix)]
 
-    def percent(c):
-        return f'{c*100/total:.2f}%'
-    print(f'Total de pares = {total}\nIguais a 0: {e0}\n--')
-    print(f'Entre 0 e 0.25: {p25} ({percent(p25)})\nEntre 0.25 e 0.50: {p50} ({percent(p50)})\n'
-          f'Entre 0.50 e 0.75: {p75} ({percent(p75)})\nEntre 0.75 e 1: {p1} ({percent(p1)})\n--')
-    print(f'Entre 0 e -0.25: {n25} ({percent(n25)})\nEntre -0.25 e -0.50: {n50} ({percent(n50)})\n'
-          f'Entre -0.50 e -0.75: {n75} ({percent(n75)})\nEntre -0.75 e -1: {n1} ({percent(n1)})\n--')
+    mean_correlations = np.mean(matrix_without_nan)
+    intervals = [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1]
+    frequency, _ = np.histogram(matrix_without_nan, bins=intervals)
 
-    mean_correlation = np.mean(correlations)
-    print(f'A correlação média é de {mean_correlation}\n------------')
+    total = len(matrix_without_nan)
+
+    num_zeros = np.count_nonzero(matrix_without_nan == 0)
+    num_positive_one = np.count_nonzero(matrix_without_nan == 1)
+    num_negative_one = np.count_nonzero(matrix_without_nan == -1)
+
+    percent = lambda freq: f'({freq*100/total:.2f}%)'
+
+    print(f'Num of pairs equal to -1 is {num_negative_one} {percent(num_negative_one)}')
+    print(f'Num of pairs between -1 and -0.75 is {frequency[0]} {percent(frequency[0])}')
+    print(f'Num of pairs between -0.75 and -0.50 is {frequency[1]} {percent(frequency[1])}')
+    print(f'Num of pairs between -0.50 and -0.25 is {frequency[2]} {percent(frequency[2])}')
+    print(f'Num of pairs between -0.25 and 0 is {frequency[3]} {percent(frequency[3])}')
+    print(f'Num of pairs equal to 0 is {num_zeros} {percent(num_zeros)}')
+    print(f'Num of pairs between 0 and 0.25 is {frequency[4]} {percent(frequency[4])}')
+    print(f'Num of pairs between 0.25 and 0.50 is {frequency[5]} {percent(frequency[5])}')
+    print(f'Num of pairs between 0.50 and 0.75 is {frequency[6]} {percent(frequency[6])}')
+    print(f'Num of pairs between 0.75 and 1 is {frequency[7]} {percent(frequency[7])}')
+    print(f'Num of pairs equal to 1 is {num_positive_one} {percent(num_positive_one)}')
+
+    print(f'The mean correlation is {mean_correlations}\n------------')
+
+    confidence = 0.95
+    std_error = np.std(matrix_without_nan, ddof=1) / np.sqrt(total)
+    confidence_interval = t.interval(confidence, df=total-1, loc=mean_correlations, scale=std_error)
+
+    return mean_correlations, confidence_interval
 
 
-class SampleAnalysis:
+class SampleCorrelation:
 
-    def __init__(self, dataset: Dataset, interval: int):
+    def __init__(self, dataset: Dataset):
         self.dataset = dataset
         self.f3_dm = Path.f3_dm(dataset.name)
-        self.data = self.load_interval(interval)
+        self.data = None
 
     def load_interval(self, interval: int):
         data = np.array([])
@@ -109,12 +97,11 @@ class SampleAnalysis:
                     data = user_sample
                 else:
                     data = np.vstack((data, user_sample))
-        return data
+        self.data = data
 
     def pearson_correlation_at_interval(self):
         pearson_correlation_matrix = np.corrcoef(self.data)
         print('Pearson Correlation')
-        most_correlated(pearson_correlation_matrix)
         return pearson_correlation_matrix
 
     def spearman_correlation_at_interval(self):
@@ -126,7 +113,6 @@ class SampleAnalysis:
                 spearman_correlation_matrix[i, j], _ = spearmanr(self.data[i], self.data[j])
 
         print('Spearman Correlation')
-        most_correlated(spearman_correlation_matrix)
         return spearman_correlation_matrix
 
     def kendaltau_correlation_at_interval(self):
@@ -138,5 +124,31 @@ class SampleAnalysis:
                 coeficiente, _ = kendalltau(self.data[i], self.data[j])
                 kendaltau_correlation_matrix[i, j] = coeficiente
         print('Kendaltau Correlation')
-        most_correlated(kendaltau_correlation_matrix)
         return kendaltau_correlation_matrix
+
+
+def get_sample_correlations(dataset, last_interval):
+    analysis = SampleCorrelation(dataset)
+    pearson_means = []
+    pearson_ci = []
+    spearman_means = []
+    spearman_ci = []
+    kendal_means = []
+    kendal_ci = []
+    for interval in range(last_interval):
+        analysis.load_interval(interval)
+
+        pearson_matrix = analysis.pearson_correlation_at_interval()
+        mean, ci = mean_correlation(pearson_matrix)
+        pearson_means.append(mean)
+        pearson_ci.append(ci)
+
+        spearman_matrix = analysis.spearman_correlation_at_interval()
+        mean, ci = mean_correlation(spearman_matrix)
+        spearman_means.append(mean)
+        spearman_ci.append(ci)
+
+        kendal_matrix = analysis.kendaltau_correlation_at_interval()
+        mean, ci = mean_correlation(kendal_matrix)
+        kendal_means.append(mean)
+        kendal_ci.append(ci)
