4beed5ac85bb912a29c3467cc841f7216bf7c824
Diego Souza <souza.claudiodiego@gmail.com>
2024-04-29 16:22:37 -0300

    png figures to pdf

--- inner_types/names.py
+++ inner_types/names.py
@@ -4,19 +4,19 @@ from enum import Enum
 class ExportedFiles(Enum):
     TRAINING_LOSS_CSV = 'Training losses.csv'
     TESTING_LOSS_CSV = 'Testing losses.csv'
-    LOSSES_PNG = 'Losses curves.png'
+    LOSSES_PNG = 'Losses curves.pdf'
 
     CONTACT_TIME_CSV = 'Contact time.csv'
     MSE_CSV = 'MSE.csv'
     SSIM_CSV = 'SSIM.csv'
     ARI_CSV = 'ARI.csv'
 
-    CONTACT_TIME_PNG = 'Contact time.png'
-    MSE_PNG = 'MSE.png'
-    SSIM_PNG = 'SSIM.png'
-    ARI_PNG = 'ARI.png'
+    CONTACT_TIME_PNG = 'Contact time.pdf'
+    MSE_PNG = 'MSE.pdf'
+    SSIM_PNG = 'SSIM.pdf'
+    ARI_PNG = 'ARI.pdf'
 
     KS_CHOSEN = 'ks_chosen.csv'
 
-    CORRELATION = 'Samples Correlation.png'
-    MCORRELATION = 'Correlation Matrix.png'
+    CORRELATION = 'Samples Correlation.pdf'
+    MCORRELATION = 'Correlation Matrix.pdf'
