1cda2231afc4c02b50552519b52813c4740630b7
Diego Souza <souza.claudiodiego@gmail.com>
2024-08-16 10:34:18 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -11,14 +11,14 @@ python experiment_script.py --dataset ngsim --approach cen --strategy acc --last
 python experiment_script.py --dataset ngsim --approach cen --strategy sli --last_interval 10
 
 elif [ "$1" == "rt" ]; then
-python experiment_script.py --dataset rt --approach fed --strategy acc --last_interval 42
-python experiment_script.py --dataset rt --approach fed --strategy sli --last_interval 42
+python experiment_script.py --dataset rt --approach fed --strategy acc --last_interval 40
+python experiment_script.py --dataset rt --approach fed --strategy sli --last_interval 40
 
-python experiment_script.py --dataset rt --approach fed --strategy acc --last_interval 42 --proximal_term 1.0
-python experiment_script.py --dataset rt --approach fed --strategy sli --last_interval 42 --proximal_term 1.0
+python experiment_script.py --dataset rt --approach fed --strategy acc --last_interval 40 --proximal_term 1.0
+python experiment_script.py --dataset rt --approach fed --strategy sli --last_interval 40 --proximal_term 1.0
 
-python experiment_script.py --dataset rt --approach cen --strategy acc --last_interval 42
-python experiment_script.py --dataset rt --approach cen --strategy sli --last_interval 42
+python experiment_script.py --dataset rt --approach cen --strategy acc --last_interval 40
+python experiment_script.py --dataset rt --approach cen --strategy sli --last_interval 40
 
 elif [ "$1" == "sfc" ]; then
 python experiment_script.py --dataset sfc --approach fed --strategy acc --last_interval 42
