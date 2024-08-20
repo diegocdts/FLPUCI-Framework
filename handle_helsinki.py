b34dc4b45b2fb38c91b4bcd19660d7d4fae5cb91
Diego Souza <souza.claudiodiego@gmail.com>
2024-08-20 12:59:16 -0300

    fix

--- handle_helsinki.py
+++
@@ -1,6 +0,0 @@
-from helpful_scripts.new_dataset import NewDataset
-
-path = '/home/diegocdts/eclipse/eclipse-workspace/One6/src/reports/default_scenario_MovementNs2Report.txt'
-
-helsinki = NewDataset('helsinki', path)
-helsinki.export_raw_data()
