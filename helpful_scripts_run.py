9834c2dbd1c846a29e8563e51c4e076df1fc2095
Diego Souza <souza.claudiodiego@gmail.com>
2024-08-27 12:57:14 -0300

    helpful_scripts_run

--- helpful_scripts_run.py
+++ helpful_scripts_run.py
@@ -1,5 +1,13 @@
 from app_validation.opportunistic_routing import RoutingMetricAnalysis
+from helpful_scripts.new_dataset import NewDataset
+
+"""
+nd = NewDataset('helsinki', '/home/diegocdts/eclipse/eclipse-workspace/One6/src/reports/movement/mobility_generator_helsinki_MovementNs2Report.txt')
+nd.export_raw_data()
+
+nd = NewDataset('manhattan', '/home/diegocdts/eclipse/eclipse-workspace/One6/src/reports/movement/mobility_generator_manhattan_MovementNs2Report.txt')
+nd.export_raw_data()
+"""
 
 rma = RoutingMetricAnalysis('/home/diegocdts/eclipse/eclipse-workspace/One6/src/reports')
-
 rma.metrics()
