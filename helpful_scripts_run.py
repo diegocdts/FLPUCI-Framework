f24294f950f6642722cd5ea240f5a57ba32551d7
Diego Souza <souza.claudiodiego@gmail.com>
2024-09-02 14:27:16 -0300

    new data

--- helpful_scripts_run.py
+++ helpful_scripts_run.py
@@ -2,10 +2,10 @@ from app_validation.opportunistic_routing import RoutingMetricAnalysis
 from helpful_scripts.new_dataset import NewDataset
 
 
-nd = NewDataset('helsinki', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/movement/helsinki_MovementNs2Report.txt')
+nd = NewDataset('helsinki', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/helsinki/helsinki_MovementNs2Report.txt')
 nd.export_raw_data()
 
-nd = NewDataset('manhattan', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/movement/manhattan_MovementNs2Report.txt')
+nd = NewDataset('manhattan', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/manhattan/manhattan_MovementNs2Report.txt')
 nd.export_raw_data()
 
 rma = RoutingMetricAnalysis('/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/manhattan')
