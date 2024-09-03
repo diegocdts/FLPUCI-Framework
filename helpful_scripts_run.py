11ea61221393707bfb550000bcdf72ffdc4191c4
Diego Souza <souza.claudiodiego@gmail.com>
2024-09-03 11:29:35 -0300

    helpful

--- helpful_scripts_run.py
+++ helpful_scripts_run.py
@@ -2,14 +2,14 @@ from app_validation.opportunistic_routing import RoutingMetricAnalysis
 from helpful_scripts.new_dataset import NewDataset
 
 
-nd = NewDataset('helsinki', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/helsinki/helsinki_MovementNs2Report.txt')
+nd = NewDataset('helsinki', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/helsinki/movement/helsinki_MovementNs2Report.txt')
 nd.export_raw_data()
 
-nd = NewDataset('manhattan', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/manhattan/manhattan_MovementNs2Report.txt')
+nd = NewDataset('manhattan', '/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/manhattan/movement/manhattan_MovementNs2Report.txt')
 nd.export_raw_data()
 
 rma = RoutingMetricAnalysis('/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/manhattan')
 rma.metrics()
 
-rma = RoutingMetricAnalysis('/home/diegocdts/eclipse/eclipse-workspace/One6/src/reports/helsinki')
+rma = RoutingMetricAnalysis('/home/diegocdts/eclipse/eclipse-workspace2/ONE/src/reports/helsinki')
 rma.metrics()
